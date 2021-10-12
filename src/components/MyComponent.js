deb02c262616e4984d62bd2d82fe84aacab3f249
Fupin Yao <xcmax@qq.com>
2021-10-11 19:08:56 -0700

    d1

---
+++ src/components/MyComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class MyComponent extends React.Component {
+  render() {
+    return <div><input onChange={(e)=>{this.setState({input: e.target.value})}} 
+    type="text"/> </div>;
+  }
+}
